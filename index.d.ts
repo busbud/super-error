6fb1e8eecb1e10dd6d66760f1dd8c95bedf888a0
Dustin Blackman <dev@dustinblackman.com>
2017-09-15 11:23:47 -0400

    add typings

---
+++ index.d.ts
@@ -0,0 +1,21 @@
+declare interface SuperErrorI {
+  name: string;
+  message: string;
+  [k: string]: any;
+
+  new(...args: any[]): SuperError;
+}
+
+declare class SuperError extends Error {
+  name: string;
+  message: string;
+  [k: string]: any;
+
+  static subclass(exports: any, name: string, subclass_constructor: (this: SuperError, ...args: any[]) => void): SuperErrorI;
+  static subclass(name: string, subclass_constructor: (this: SuperError, ...args: any[]) => void): SuperErrorI;
+
+  constructor(...args: any[]);
+  causedBy(err: Error): this;
+}
+
+export = SuperError;

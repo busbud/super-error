c8264736d2c3e99f02872db7a5a67120fb7a7d09
Jacob Cook <jacob@peakwinter.net>
2019-08-19 12:44:23 -0400

    Create module in TS declaration

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-declare module 'super-error' {
+declare namespace SuperError {
   interface SuperErrorI {
     name: string;
     message: string;
@@ -6,18 +6,21 @@ declare module 'super-error' {
   
     new(...args: any[]): SuperError;
   }
-  
-  class SuperError extends Error {
-    name: string;
-    message: string;
-    [k: string]: any;
-  
-    static subclass(name: string): SuperErrorI;
-    static subclass(exports: any, name: string): SuperErrorI;
-    static subclass(exports: any, name: string, subclass_constructor: (this: SuperError, ...args: any[]) => void): SuperErrorI;
-    static subclass(name: string, subclass_constructor: (this: SuperError, ...args: any[]) => void): SuperErrorI;
-  
-    constructor(...args: any[]);
-    causedBy(err: Error): this;
-  }
 }
+
+interface SuperError extends Error {
+  name: string;
+  message: string;
+  [k: string]: any;
+
+  static subclass(name: string): SuperErrorI;
+  static subclass(exports: any, name: string): SuperErrorI;
+  static subclass(exports: any, name: string, subclass_constructor: (this: SuperError, ...args: any[]) => void): SuperErrorI;
+  static subclass(name: string, subclass_constructor: (this: SuperError, ...args: any[]) => void): SuperErrorI;
+
+  constructor(...args: any[]);
+  causedBy(err: Error): this;
+}
+
+declare var SuperError: SuperError;
+export = SuperError;
